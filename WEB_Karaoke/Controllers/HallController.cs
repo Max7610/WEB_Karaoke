51f3bb338f3c2e0c925ebf68d27a1391de44a085
Max7610 <56383853+Max7610@users.noreply.github.com>
2025-01-07 22:06:46 +0300

    пробую модель

--- WEB_Karaoke/Controllers/HallController.cs
+++ WEB_Karaoke/Controllers/HallController.cs
@@ -1,12 +1,26 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WEB_Karaoke.Models;
 
 namespace WEB_Karaoke.Controllers
 {
     public class HallController : Controller
     {
+        List<Visitor> _clientList = new List<Visitor>()
+        {
+            new Visitor {Id = 1,UserName = "Gek", MyStatus = Visitor.Status.Gold,SongList = new List<string>(){"Soung 1", "Soung 2" } },
+            new Visitor {Id = 1,UserName = "Don", MyStatus = Visitor.Status.Gold,SongList = new List<string>(){"Soung 3", "Soung 4" } },
+            new Visitor {Id = 1,UserName = "Max", MyStatus = Visitor.Status.Gold,SongList = new List<string>(){"Soung 5", "Soung 6" } }
+
+        };
+
+
         public IActionResult Index()
         {
-            return View();
+            return View(_clientList);
+        }
+        public IActionResult Soung(string Id)
+        {
+            return Content($"Hall controller >> {Id} <<");
         }
     }
 }
---
+++ WEB_Karaoke/Models/Visitor.cs
@@ -0,0 +1,21 @@
+﻿namespace WEB_Karaoke.Models
+{
+    public class Visitor
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+
+        public enum Status { Gold, Silver }
+        public Status MyStatus { get; set; }
+        public List<string> SongList = new List<string>() ;
+        public string ToString
+        {
+            get
+            {
+                return $"id- {Id} Name-{UserName}  MyStatus-{MyStatus}";
+            }
+        }
+
+    }
+}
+ 
