440de9e3f9f4b83c859336dff8350f43528f6303
Max7610 <56383853+Max7610@users.noreply.github.com>
2025-01-07 20:58:06 +0300

    Пустой шаблон

---
+++ WEB_Karaoke/Controllers/HallController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WEB_Karaoke.Controllers
+{
+    public class HallController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
